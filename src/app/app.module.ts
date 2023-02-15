d1ce3e21d48734912130310d6cc9aabda06662bd
carlos2172167 <ramirezcar710@gmail.com>
2023-02-14 22:51:44 -0500

    perfil

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,13 @@ import { NativeScriptModule } from '@nativescript/angular'
 
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
+import { InicioComponent } from './home/inicio/inicio.component'
 import { RegisterComponent } from './home/register/register.component'
 
 @NgModule({
   bootstrap: [AppComponent],
   imports: [NativeScriptModule, AppRoutingModule],
-  declarations: [AppComponent, RegisterComponent],
+  declarations: [AppComponent, RegisterComponent, InicioComponent],
   schemas: [NO_ERRORS_SCHEMA],
 })
 export class AppModule {}
