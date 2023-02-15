d5ac116b70fca5c56d97359fcf1c9d3bdb7ca061
Camilo Ciro Orozco <camilo2190055@correo.uis.edu.co>
2023-02-15 01:57:21 -0500

    main(6): home

--- src/app/home/inicio/inicio.component.ts
+++ src/app/home/inicio/inicio.component.ts
@@ -7,6 +7,11 @@ import { Page } from '@nativescript/core';
   styleUrls: ['./inicio.component.css'],
 })
 export class InicioComponent implements OnInit {
+  img = 1;
+  btn1 = true;
+  btn2 = true;
+  btn3 = true;
+  pos = true;
   constructor(private page:Page) {
     // Use the component constructor to inject providers.
   }
@@ -16,7 +21,23 @@ export class InicioComponent implements OnInit {
     // Init your component properties here.
   }
 
-  prueba() {
-    alert()
+  changePos(){
+    this.pos = !this.pos;
+  }
+
+  changeColor1(){
+    this.btn1 = !this.btn1;
+  }
+  changeColor2(){
+    this.btn2 = !this.btn2;
+  }
+  changeColor3(){
+    this.btn3 = !this.btn3;
+  }
+
+  changeImg() {
+    if (this.img <= 1) {
+      this.img = this.img + 1;
+    }
   }
 }
