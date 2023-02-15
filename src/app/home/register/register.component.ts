6c3889cc90ef77ddc5cab96e5dedb05e82a2a80a
Camilo Ciro Orozco <camilo2190055@correo.uis.edu.co>
2023-02-14 21:47:58 -0500

    main(2): register component

--- src/app/home/register/register.component.ts
+++ src/app/home/register/register.component.ts
@@ -5,10 +5,16 @@ import { Page } from '@nativescript/core';
   selector: 'Register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css'],
-})  
+})
 export class RegisterComponent implements OnInit {
 
   var1 = 1
+  img1 = 1
+  img2 = 1
+  img3 = 1
+  img4 = 1
+  img5 = 1
+  img6 = 1
   constructor(private page:Page) {
     // Use the component constructor to inject providers.
   }
@@ -19,5 +25,48 @@ export class RegisterComponent implements OnInit {
   }
   cambiarvar1() {
     this.var1 = 2;
+  }
+
+  changeColor1() {
+    if (this.img1 === 1) {
+      this.img1 = 2
+    } else {
+      this.img1 = 1
+    }
+  }
+  changeColor2() {
+    if (this.img2 === 1) {
+      this.img2 = 2
+    } else {
+      this.img2 = 1
+    }
+  }
+  changeColor3() {
+    if (this.img3 === 1) {
+      this.img3 = 2
+    } else {
+      this.img3 = 1
+    }
+  }
+  changeColor4() {
+    if (this.img4 === 1) {
+      this.img4 = 2
+    } else {
+      this.img4 = 1
+    }
+  }
+  changeColor5() {
+    if (this.img5 === 1) {
+      this.img5 = 2
+    } else {
+      this.img5 = 1
+    }
+  }
+  changeColor6() {
+    if (this.img6 === 1) {
+      this.img6 = 2
+    } else {
+      this.img6 = 2
+    }
   }
 }
