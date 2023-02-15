39d20c450d055796158d1d610f589416fe6801e4
carlos2172167 <ramirezcar710@gmail.com>
2023-02-14 21:36:16 -0500

    inicio

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core'
 import { Routes } from '@angular/router'
 import { NativeScriptRouterModule } from '@nativescript/angular'
 import { RegisterComponent } from './home/register/register.component'
+import { InicioComponent } from './home/inicio/inicio.component'
+
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   {path: 'register', component: RegisterComponent},
+  {path: 'inicio', component: InicioComponent},
   {
     path: 'home',
     loadChildren: () => import('~/app/home/home.module').then((m) => m.HomeModule),
